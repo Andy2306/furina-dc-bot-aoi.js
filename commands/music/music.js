f6bdcad072a9e5019be57a9fbe10430881960e5c
andy <72691938+Andy2306@users.noreply.github.com>
2024-10-06 17:29:13 +0000

    add bugs

--- commands/music/music.js
+++ commands/music/music.js
@@ -1,55 +1,7 @@
-const { prototype } = require("opusscript");
-
-module.exports = [
-  {
-    name: "play",
-    aliases: "p",
-    code: `
-    $title[💿 Sedang memutar musik] 
-    $description[[$songInfo[title] - $humanizeMs[$songInfo[duration]]](https://www.youtube.com/watch?v=$songInfo[id])]
-    $thumbnail[$songInfo[thumbnail]]
-    $footer[Requested By: $username]
-    $color[Random]
-    $addTimestamp
-    $playTrack[$message;youtube]
-    $joinVc
-    $addButton[1;🚫;danger;stop]
-    $addButton[1;⏸️;primary;pause]
-    $addButton[1;▶️;primary;resume]
-    $addButton[1;🔄️;primary;loop]`,
-  },
-  {
-    name: "pause",
-    type: "interaction",
-    prototype: "button",
-    code: `
-    $pauseTrack
-    $interactionReply[Lagu dijeda!]`,
-  },
-  {
-    name: "resume",
-    type: "interaction",
-    prototype: "button",
-    code: `
-    $resumeTrack
-    $interactionReply[Lagu dilanjutkan !]`,
-  },
-  {
-    name: "stop",
-    type: "interaction",
-    prototype: "button",
-    code: `
-    $stopTrack
-	  $leaveVC
-    $interactionReply[Lagu dihentikan!]`,
-  },
-
-  {
-    name: "loop",
-    type: "interaction",
-    prototype: "button",
-    code: `
-    $loopMode[song]
-    $interactionReply[Lagu akan di loop!]`
-  }
-];
+module.exports = ({
+  name: "play",
+  code: `
+  $playTrack[ambulance mcr;youtube]
+  $joinVc
+`
+})
--- index.js
+++ index.js
@@ -1,75 +1,46 @@
 require('dotenv').config();
 const { AoiClient } = require("aoi.js");
-const { AoiVoice, PlayerEvents, PluginName, Cacher, Filter } = require("@akarui/aoi.music");
+const { AoiVoice, PlayerEvents, PluginName, Cacher, Filter } = require("@aoijs/aoi.music");
 const { LoadCommands } = require("aoi.js");
 
-const bot = new AoiClient({
+const client = new AoiClient({
   token: process.env.TOKEN,
   prefix: process.env.PREFIX,
   intents: ["MessageContent", "Guilds", "GuildMessages", "GuildVoiceStates"],
   events: ["onMessage", "onInteractionCreate"],
   database: {
     type: "aoi.db",
-    db: require("@akarui/aoi.db"),
+    db: require("@aoijs/aoi.db"),
     dbType: "KeyValue",
     tables: ["main"],
     securityKey: "a-32-characters-long-string-here",
   },
 });
 
-const voice = new AoiVoice(bot, {
-  searchOptions: {
-    soundcloudClientId: "Soundcloud ID", // optional
-    youtubegl: "US",
-  },
+const voice = new AoiVoice(client, {
   requestOptions: {
     offsetTimeout: 0,
     soundcloudLikeTrackLimit: 200,
   },
+  searchOptions: {
+   youtubeClient: "WEB"
+  }
 });
 
 // Command Example (ping)
-
-bot.command({
+client.command({
   name: "ping",
-  code: `
-  $description[
-  Pong! $pingms
-  OS: $cpu[os]
-  Process: $cpu[process]
-  Ram: \`$ram%\` dari \`$maxRam%\`
-  ]`,
+  code: `Pong! $pingms`,
 });
-
-// Bot variables
-
-bot.variables({
-  test: 0,
-  FishRandomNumber: 0,
-  FishRandomString: "",
-  
-});
-
-// Bot status
-bot.status({
-  name: "Sesuatu...",
-  type: "STREAMING",
-  time: 25,
-  url: "https://github.com/Andy2306/furina-dc-bot-aoi.js"
-});
-
 
 // optional (cacher / filter)
-
 voice.addPlugin(PluginName.Cacher, new Cacher("memory" /* or "disk" */));
-voice.addPlugin(
-  PluginName.Filter,
-  new Filter({
-    filterFromStart: false,
-  }),
-);
-voice.bindExecutor(bot.functionManager.interpreter);
+voice.addPlugin(PluginName.Filter, new Filter({
+    filterFromStart: false
+})),
 
-bot.loadCommands("./commands/", true);
-const loader = new LoadCommands(bot);
+voice.bindExecutor(client.functionManager.interpreter);
+
+client.loadCommands("./commands/", true);
+const loader = new LoadCommands(client);
 loader.load(voice.cmds, "./voice/");
